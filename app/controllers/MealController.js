152cc5cbad08b3b858e68d8d4baa8cc6dfe44622
Keffrey <keffkahouse@gmail.com>
2023-05-02 21:59:12 +0300

    menu completed

--- app/controllers/MealController.js
+++ app/controllers/MealController.js
@@ -37,7 +37,37 @@ class MealController {
 
     // TODO: fetch a meal from a specific caterer
     fetchCatererMeals(){
+        this.app.get('/meals/caterer_meals', async (req, res) => {
+            const name = req.body.name;
 
+            try {
+                const caterer = await User.findOne({
+                    where: {
+                        name: name,
+                        isCaterer: yes
+                    }
+                })
+
+                const catererId = caterer.id
+                
+                const caterer_meals = await Meal.findAll({
+                    where: {
+                        caterer: catererId            
+                    }
+                })
+
+                res.json({
+                    "status": "successful",
+                    "data": caterer_meals
+                })
+            } catch (error) {
+                res.status(422).json({
+                    "status": "failed",
+                    "message": error.message
+                })
+            }
+
+        })
     }
 
     // create a meal
@@ -48,27 +78,26 @@ class MealController {
                 const uid = parseInt(req.params.uid);
                 const { name, price, description } = req.body;
 
-                const caterer = await User.findByPk(uid);
-
                 // TODO: Check if user is caterer
+                const caterer = await User.findOne({
+                    where: {
+                        id: uid,
+                        isCaterer: yes
+                    }
+                })
 
-                if(caterer){
-                    const meal = await Meal.create({
-                        name: name,
-                        price: price,
-                        description: description,
-                        caterer: uid
-                    });
-                    res.json({
-                        "status": "success",
-                        "data": meal
-                    })
-                }else{
-                    res.status(404).json({
-                        "status": "failed",
-                        "message": "You do not seem to be a caterer"
-                    })
-                }
+                const meal = await Meal.create({
+                    name: name,
+                    price: price,
+                    description: description,
+                    caterer: caterer.id
+                });
+
+                res.json({
+                    "status": "success",
+                    "data": meal
+                })
+
             }catch(error){
                 res.status(500).json({
                     "status": "failed",
@@ -80,12 +109,62 @@ class MealController {
 
     //TODO: update a meal
     updateMeal(){
+        this.app.put('/meals/update_meal/:uid', async (req, res) => {
+            const uid = parseInt(req.params.uid);
+            const { name, price, description } = req.body;
 
+            try {
+                
+                await Meal.update({
+                    name: name,
+                    price: price,
+                    description: description
+                }, {
+                    where: {
+                        id: uid
+                    }
+                })
+
+                res.json({
+                    "status": "successful",
+                    "message": "successfully updated meal"
+                })
+
+            } catch (error) {
+                res.status(422).json({
+                    "status": "failed",
+                    "message": error.message
+                })
+            }
+
+        })
     }
 
     //TODO: delete a meal
     deleteMeal(){
-        
+        this.app.delete("/meals/delete_meal/:id", async (req, res) => {
+            const mealId = parseInt(req.params.id);
+             
+            try {
+                await Meal.destroy({
+                    where: {
+                        id: mealId
+                    }
+                });
+
+                res.json({
+                    "status": "success",
+                    "message": "meal deleted"
+                })
+
+            } catch (error) {
+                res.status(422).json({
+                    "status": "failed",
+                    "message": error.message
+                })
+            }
+            
+        })
     }
 
 }
--- app/controllers/MenuController.js
+++ app/controllers/MenuController.js
@@ -18,22 +18,115 @@ class MenuController {
 
     // create a menu
     createMenu(){
+        this.app.post("/create_menu/:uid", async (req, res) => {
+            const uid = parseInt(req.params.uid)
+            const { name, description, meals, isMealOfDay } = req.body;
 
+            try {
+                const caterer = await User.findOne({
+                where: {
+                    id: uid,
+                    isCaterer: yes
+                }
+            })
+
+            const caterer_id = caterer.id;
+
+            await Menu.create({
+                name: name,
+                description: description,
+                meals: meals,
+                isMealOfDay: isMealOfDay
+            })
+
+            res.json({
+                "status": "success",
+                "message": "menu created"
+            })
+                
+            } catch (error) {
+                res.status(422).json({
+                    "status": "failed",
+                    "message": error.message
+                })
+                
+            }
+            
+
+        })
     }
 
     // fetch all menus
     fetchMenus(){
+        this.app.get("/fetch_menu", async (req, res) => {
 
+            const menu_list = await Menu.findAll()
+
+            res.json({
+                "status": "successful",
+                "data": menu_list
+            })
+
+        })
     }
 
     // update a menu
     updateMenu(){
+        this.app.put("/update_menu", async (req, res) => {
+            const { uid, name, description, meals, isMealOfDay } = req.body;
+            const menu_id = parseInt(uid);
 
+            try {
+                await Menu.update({
+                    name: name,
+                    description: description,
+                    meals: meals,
+                    isMealOfDay: isMealOfDay
+                }, {
+                    where: {
+                        id: menu_id
+                    }
+                })
+
+                res.json({
+                    "status": "success",
+                    "message": "menu updated successfully"
+                })
+            } catch (error) {
+                res.status(422).json({
+                    "status": "failed",
+                    "message": error.message
+                })
+            }
+
+        })
     }
 
     // delete a menu
     deleteMenu(){
+        this.app.delete("/delete_menu", async (req, res) => {
+            const uid = req.body.uid;
+            const menu_id = parseInt(uid);
 
+            try {
+                await Menu.destroy({
+                    where: {
+                        id: menu_id
+                    }
+                })
+
+                res.json({
+                    "status": "success",
+                    "message": "menu deleted successfully"
+                })
+            } catch (error) {
+                res.status(422).json({
+                    "status": "failed",
+                    "message": error.message
+                })
+            }
+
+        })
     }
 
 }
